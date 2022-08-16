dcfa8e81f301c5b8609bc45cf7b332762bd1230b
Alexandr Virodov <alexandr.virodov@uky.edu>
2022-08-16 12:50:37 -0400

    make golden data from current run - for regression testing.

--- test/cases/philips-isyntax/golden-image-data/enhance.sh
+++
@@ -1,5 +0,0 @@
-set -e -x
-
-for level in 7 6 5 4 3 2 1 0; do
-  convert -brightness-contrast -70x80 testslide_0_0_4096_4096_$level.png enhanced_testslide_0_0_4096_4096_$level.png
-done
--- test/cases/philips-isyntax/run_extract.sh
+++ test/cases/philips-isyntax/run_extract.sh
@@ -1,6 +1,9 @@
 set -e -x
 WRITE_PNG=../../../build/tools/openslide-write-png
 
+# NOTE: the file naming convention corresponds to coordinates that have the pixels of interest in the .tif extracted using the SDK. There are various offsets (765px fixed, +/-1px per level due to different roundings), so
+# the subimages are extracted with the correct coords in the .isyntax, and named with the corresponding coords in .tiff.
+
 compute_diff() {
   composite golden-image-data/$1 extracted-image-data/$1 -compose difference extracted-image-data/diff_$1
   convert extracted-image-data/diff_$1 -auto-level extracted-image-data/diffnorm_$1
@@ -10,6 +13,7 @@ compute_diff() {
 
 write_png_whole() {
   $WRITE_PNG golden-image-data/testslide.isyntax 0 0 $1 $2 $3 extracted-image-data/testslide_0_0_37376_73216_$1.png
+  compute_diff testslide_0_0_37376_73216_$1.png
 }
 
 write_png_part1() {
@@ -40,36 +44,32 @@ write_png_corner_enhanced 765 765 5 128 128
 write_png_corner_enhanced 765 765 6 64 64
 write_png_corner_enhanced 765 765 7 32 32
 
+write_png_whole 7 292 572
+write_png_whole 6 584	1144
+write_png_whole 5 1168	2288
+write_png_whole 4 2336	4576
+write_png_whole 3 4672	9152
 
-# TODO(avirodov): this is a bit more blurry than golden, seems some resampling is going on
-# write_png_corner_enhanced 763 763 1 2048 2048
-# This is better - no resampling, but offset:
-# write_png_corner_enhanced 768 768 1 2048 2048
+write_png_part1 19965 19965 0 2048 2048
+write_png_part1 19965 19965 1 1024 1024
+write_png_part1 19965 19965 2 512 512
+write_png_part1 19965 19965 3 256 256
+write_png_part1 19965 19965 4 128 128
+write_png_part1 19965 19965 5 64 64
+write_png_part1 19965 19965 6 32 32
+write_png_part1 19965 19965 7 16 16
 
 
 
-
-
-
-
-
-#write_png_whole 7 292 572
-#write_png_whole 6 584	1144
-#write_png_whole 5 1168	2288
-#write_png_whole 4 2336	4576
-#write_png_whole 3 4672	9152
 
 # In golden, a point is at 112,893
 # if extracting at 19200,19200, same point is at 877, 1658, so offset of 765 in both directions.
+# TODO(avirodov): why not 19200?
 #write_png_part1 19965 19965 0 2048 2048
 
 # In golden, a point is at 55,446
 # if extracting at 15000,15000, same point is at 2537, 2928, so offset of 2482 in both directions.
 # write_png_part1 9982 9982 1 4096 4096
-
-
-
-
 
 
 # TODO(avirodov): why this generates empty slide for tiff?
@@ -86,19 +86,5 @@ write_png_corner_enhanced 765 765 7 32 32
 #write_png_part1_tiff 19060 19060 7 16 16
 
 
-# TODO(avirodov): why not 19200?
-# write_png_part1 19965 19965 0 2048 2048
-
-
-#write_png_part1 19200 19200 0 2048 2048
-#write_png_part1 19198 19198 1 1024 1024
-#write_png_part1 19192 19192 2 512 512
-#write_png_part1 19184 19184 3 256 256
-#write_png_part1 19180 19180 4 128 128
-#write_png_part1 19160 19160 5 64 64
-#write_png_part1 19120 19120 6 32 32
-#write_png_part1 19060 19060 7 16 16
-#
-
 cat difflog.txt
-
+echo num_zero_difference=`cat difflog.txt | grep 'min=0 max=0' | wc -l` num_cases=`cat difflog.txt | grep 'png' | wc -l`
