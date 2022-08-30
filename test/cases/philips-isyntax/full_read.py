ae39f8b0a7efbc07c229e87081ef75ee13361603
Alexandr Virodov <Alexandr.Virodov@uky.edu>
2022-08-30 20:21:26 +0000

    Fix bug where there was not enough space in the variable to store bit operation result (u8, but need more than 8 bits). This resulted in a second consecutive zero-run symbol not being recognized, and decoding failing from there on.

---
+++ test/cases/philips-isyntax/full_read.py
@@ -0,0 +1,38 @@
+import openslide
+import numpy as np
+from enum import Enum
+import sys
+
+if __name__ == "__main__":
+    """Reads the whole slide to test ML case & memory consumption."""
+    # TODO(avirodov): args.
+    level = int(sys.argv[1])
+    slide_path = sys.argv[2]
+
+    print(f'{slide_path=}')
+    print(f'{level=}')
+    
+    slide = openslide.open_slide(slide_path)
+    tile_size = 224
+    random_offset = (0, 0)
+
+    np.random.seed(1)
+
+
+    sum_pixels = 0
+    level_dimensions = slide.level_dimensions[level]
+    num_tiles_x = level_dimensions[0] // tile_size
+    num_tiles_y = level_dimensions[1] // tile_size
+    print(f'{num_tiles_x=} {num_tiles_y=}')
+    for j in range(num_tiles_y):
+        for i in range(num_tiles_x):
+            print(f'reading {level=} {i=} {j=}')
+            image = slide.read_region((i * tile_size, j * tile_size), level, (tile_size, tile_size))
+            sum_pixels += np.sum(np.array(image))
+    print(f'{sum_pixels=}')
+
+
+
+
+
+
